a20e16af8d365172e96a55275fac6553bdbdf1e3
asdf <alexrayo.net@gmail.com>
2025-05-13 10:49:07 -0600

    working with gemini api

---
+++ babel.config.js
@@ -0,0 +1,12 @@
+console.log('📦 cargando babel.config.js…');
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    env: {
+      production: {
+        plugins: ['react-native-paper/babel'],
+      },
+    },
+  };
+};
